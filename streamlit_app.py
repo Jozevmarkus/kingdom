0eec46788962e61deae936db2c1911ed487d84dc
Mahmud <117464326+Jozevmarkus@users.noreply.github.com>
2024-10-05 19:41:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,6 +101,8 @@ st.dataframe(df_prediction_proba,
                  min_value=0,
                  max_value=1
                ),
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(str(penguins_species[prediction][0]))
              }, hide_index=True)
 
 
