8aa33ac69f0362e87315ed6b3721f6bddd189207
Mahmud <117464326+Jozevmarkus@users.noreply.github.com>
2024-10-05 18:59:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
-
+import pandas as pd
 st.title('😘 Mahmudov Mahmudzon')
 
 st.write('Hello world!')
+with st.expander('Initial data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
+
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+
