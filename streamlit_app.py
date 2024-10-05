95c99015c8a196566181af07d95d13fdc549eece
Mahmud <117464326+Jozevmarkus@users.noreply.github.com>
2024-10-05 19:43:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-st.title('😘 Mahmudov Mahmudjon')
+from sklearn.ensamble import RandomForestClassifier
+st.title('❤ Mahmudov Mahmudjon')
 
 st.write('Hello world!')
+
 with st.expander('Initial data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   
@@ -18,17 +19,17 @@ with st.expander('Initial data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+
 with st.sidebar:
-  st.header('Inout Features')
-  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
-  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
-  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('male', 'female'))
-
-
-# Create a DataFrame for the input features
+  
+  # Create a DataFrame for the input features
   data = {'island': island,
           'bill_length_mm': bill_length_mm,
           'bill_depth_mm': bill_depth_mm,
@@ -101,8 +102,6 @@ st.dataframe(df_prediction_proba,
                  min_value=0,
                  max_value=1
                ),
-penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
-st.success(str(penguins_species[prediction][0]))
              }, hide_index=True)
 
 
