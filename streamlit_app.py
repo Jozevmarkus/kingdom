7acd99001d45e9917dced46004d198cdca290da7
Mahmud <117464326+Jozevmarkus@users.noreply.github.com>
2024-10-05 19:39:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensamble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 st.title('😘 Mahmudov Mahmudjon')
 
 st.write('Hello world!')
